030d55a8efef56f306b052a757284419294ae844
QUY-FE <quysamp205@gmail.com>
2025-04-04 15:18:50 +0700

    done header

---
+++ src/main.js
@@ -0,0 +1,92 @@
+// 1. Biến
+var a = 10;
+var b = 20;
+
+
+// 2. Toán tử
+
+a = b;
+a === b; // so sánh cả kiểu dữ liệu lẫn giá trị
+a == b;// chỉ so sánh giá trị
+a > b;
+a < b;
+a >= b;
+a <= b;
+a != b;
+a + b;
+a - b;
+a * b;
+a / b;
+a % b;
+a ** b; // Lũy thừa
+
+
+// 3. Hàm
+
+var a = function(data) {
+    return data;
+}
+
+function b(data) {
+    return data;
+} 
+
+(data) => {
+    return data;
+}
+
+// 4. Cấu trúc điều khiển
+
+if(a > b){
+
+} else if(b < a) {
+    
+} else {
+    
+}
+
+while( a > b) {
+    break;
+}
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+for( var i = 0; i < a; i++) {
+    // code
+}
+
+
+
+// 5. Các đối tượng trong js 
+
+var a = [
+    {
+        id: 1,
+        name: "quy",
+        age: 19
+    },
+    {
+        id: 2,
+        name: "quy",
+        age: 19
+    }
+]
+    
+
+
+
+// 6. Kiểu dữ liệu đối tượng
+
+// 7. Các đối tượng cơ bản
+
+// 8. Đối tượng trình duyệt BOM
+
+// 9. Đối tượng Document DOM
+
